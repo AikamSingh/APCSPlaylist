3ccdf9a397c65d681afe44b07ce9aa9268f26087
Recontaminated <minecraftpro102018@gmail.com>
2023-01-16 22:23:36 -0800

    feat: wrote most of the tester

--- Playlist.java
+++ Playlist.java
@@ -9,14 +9,18 @@ public class Playlist {
      * Fields-- This will likely just need to be the ArrayList of Songs. Reference our previous problems
      * (CarDealership, Zoo) for structure on how this will look
      */
-
-
+    private ArrayList<Song> songs;
 
 
      /**
       * Constructor-- this doesn't need any parameters. You should just initialize the ArrayList and
       * then use additional methods to add Songs in one-by-one
       */
+    public Playlist()
+    {
+        songs = new ArrayList<>();
+    }
+
 
 
       
@@ -31,4 +35,47 @@ public class Playlist {
        * Determining the total duration of all songs
        * Removing all unliked songs from the playlist (careful with this one!)
        */
+    public void addSong(Song song)
+    {
+        songs.add(song);
+    }
+    public void likeSong(Song song)
+    {
+        song.setLiked(true);
+    }
+    public void removeSong(Song song)
+    {
+        songs.remove(song);
+    }
+    public String examineAllSongs() {
+        String allSongs = "";
+        for (Song song : songs) {
+            allSongs += song.to_String();
+        }
+        return allSongs;
+
+    }
+    public String examineLikedSongs() {
+        String likedSongs = "";
+        for (Song song : songs) {
+            if (song.getLiked()) {
+                likedSongs += song.to_String();
+            }
+        }
+        return likedSongs;
+    }
+    public double totalDuration() {
+        double totalDuration = 0;
+        for (Song song : songs) {
+            totalDuration += song.getLength();
+        }
+        return totalDuration;
+    }
+    public void removeUnlikedSongs() {
+        for (int i = 0; i < songs.size(); i++) {
+            if (!songs.get(i).getLiked()) {
+                songs.remove(i);
+            }
+        }
+    }
 }
--- PlaylistTester.java
+++ PlaylistTester.java
@@ -10,40 +10,59 @@ public class PlaylistTester {
     public static void main(String[] args) {
         System.out.println("Initializing a Playlist...\n");
         //Make your playlist here
-
+        Playlist playlist = new Playlist();
         System.out.println("Adding songs to the Playlist...\n");
         /**
          * Add some songs here. Note that the format for adding a Song to a Playlist p
          * is something like...
          * p.addSong(new Song(..., ..., ...))
          */
+        playlist.addSong(new Song("The Beatles", "Hey Jude", 250));
+        playlist.addSong(new Song("The Beatles", "Let It Be", 200));
+        playlist.addSong(new Song("The Beatles", "Yesterday", 150));
+        playlist.addSong(new Song("The Beatles", "In My Life", 300));
 
 
 
-        System.out.println("Printing the songs...\n");
+
         //Print out all the songs in the playlist to verify it's working correctly
+        System.out.println("Printing out all songs in the Playlist...\n");
+        System.out.println(playlist.examineAllSongs());
+
+
 
 
         System.out.println("\nLiking the songs in position X, Y, Z, etc....\n");
         //Once your songs are 'liked', this should be reflected in the next printout
+        playlist.likeSong(playlist.getSong(0));
+        playlist.likeSong(playlist.getSong(1));
+        playlist.likeSong(playlist.getSong(2));
 
 
         System.out.println("Printing the songs...\n");
+        System.out.println(playlist.examineAllSongs());
 
 
         System.out.println("\nRemoving the song in position A, B, C, etc...\n");
+        playlist.removeSong(playlist.getSong(0));
+        playlist.removeSong(playlist.getSong(1));
+        playlist.removeSong(playlist.getSong(2));
 
 
         System.out.println("Printing the songs...\n");
-
+        System.out.println(playlist.examineAllSongs());
 
         System.out.println("\nPrinting only the liked songs...\n");
         //Your Playlist should be able to do this without looping while in main!
+        System.out.println(playlist.examineLikedSongs());
+        System.out.println("--------------------");
+        System.out.println("expected: Hey Jude by The Beatles (0.06944444444444445:4.166666666666667:10.0)Let It Be by The Beatles (0.05555555555555555:3.3333333333333335:0.0)Yesterday by The Beatles (0.041666666666666664:2.5:0.0)");
+
 
 
         System.out.println("\nPrinting the total duration of all songs...\n");
-        //Note that the format should look something like minutes:seconds
-
+        //Note that the format should look something like minutes:second
+        System.out.println(playlist.totalDuration());
 
         System.out.println("\nRemoving all unliked songs from the playlist...\n");
         //This should be doable with a single method call
